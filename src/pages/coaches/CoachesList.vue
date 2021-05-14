501033afdb7d5a5bc256c6b9e8b756a2daf470ba
alperenkarate <alperensozen96@gmail.com>
2021-05-14 21:51:55 +0300

    global component, basecard is added.

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,19 +1,53 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a Coach</router-link>
-    </div>
-    <ul>
-      List of Coaches
-    </ul>
+    <base-card>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as a Coach</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
-export default {};
+import CoachItem from '../../components/coaches/CoachItem';
+export default {
+  components: {
+    CoachItem,
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches']; //second one is getter name firstt one is module napesacpe name
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
